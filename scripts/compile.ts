31e5d398acb45686bffd174e2d701a3d8c74cb5b
Svdex <ucihaibna@yahoo.com>
2026-05-24 12:35:52 +0700

    feat(client): add attachable

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -26,6 +26,10 @@ const RP_ENTRIES: JsonValidationEntry[] = [
 		url: "./schema/bedrock/client/animation/_index.json",
 	},
 	{
+		fileMatch: "attachables/**/*.{json,jsonc,json5}",
+		url: "./schema/bedrock/client/attachable/_index.json",
+	},
+	{
 		fileMatch: "entity/**/*.{json,jsonc,json5}",
 		url: "./schema/bedrock/client/entity/_index.json",
 	},
